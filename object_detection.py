c6bdfb3e34d8ebf571f4d5a99f3cb0c2567bf188
Zasraný Kapitán Trojan, vole <hyas@seznam.cz>
2023-12-09 11:59:40 +0100

    tracker from depthai

--- object_detection.py
+++ object_detection.py
@@ -29,8 +29,8 @@ config_path = f"https://raw.githubusercontent.com/luxonis/depthai-model-zoo/main
 config = requests.get(config_path).text
 config = json.loads(config)
 
-config["nn_config"]["NN_specific_metadata"]["confidence_threshold"] = 0.5
-config["nn_config"]["NN_specific_metadata"]["iou_threshold"] = 0.2
+config["nn_config"]["NN_specific_metadata"]["confidence_threshold"] = 0.3
+config["nn_config"]["NN_specific_metadata"]["iou_threshold"] = 0.05
 if "input_size" not in config["nn_config"]:
     config["nn_config"]["input_size"] = args.model.split("_")[-1]
 labelMap = config["mappings"]["labels"]
@@ -63,7 +63,7 @@ labels = nnMappings.get("labels", {})
 nnPath = args.model
 if not Path(nnPath).exists():
     print("No blob found at {}. Looking into DepthAI model zoo.".format(nnPath))
-    nnPath = str(blobconverter.from_zoo(args.model, shaves = 6, zoo_type = "depthai", use_cache=True))
+    nnPath = str(blobconverter.from_zoo(args.model, shaves = 7, zoo_type = "depthai", use_cache=True))
 # sync outputs
 syncNN = True
 
@@ -71,11 +71,20 @@ syncNN = True
 pipeline = dai.Pipeline()
 
 # Define sources and outputs
-inputVideo = pipeline.create(dai.node.XLinkIn)
+manip = pipeline.create(dai.node.ImageManip)
+objectTracker = pipeline.create(dai.node.ObjectTracker)
 detectionNetwork = pipeline.create(dai.node.YoloDetectionNetwork)
+
+manipOut = pipeline.create(dai.node.XLinkOut)
+xinFrame = pipeline.create(dai.node.XLinkIn)
+trackerOut = pipeline.create(dai.node.XLinkOut)
+xlinkOut = pipeline.create(dai.node.XLinkOut)
 nnOut = pipeline.create(dai.node.XLinkOut)
 
-inputVideo.setStreamName("inFrame")
+manipOut.setStreamName("manip")
+xinFrame.setStreamName("inFrame")
+xlinkOut.setStreamName("trackerFrame")
+trackerOut.setStreamName("tracklets")
 nnOut.setStreamName("nn")
 
 # Network specific settings
@@ -89,20 +98,46 @@ detectionNetwork.setBlobPath(nnPath)
 detectionNetwork.setNumInferenceThreads(2)
 detectionNetwork.input.setBlocking(False)
 
-# Linking
-inputVideo.out.link(detectionNetwork.input)
-detectionNetwork.out.link(nnOut.input)
+manip.initialConfig.setResizeThumbnail(W, H)
+manip.initialConfig.setFrameType(dai.ImgFrame.Type.BGR888p)
+manip.inputImage.setBlocking(True)
 
-# Connect to device and start pipeline
+objectTracker.inputTrackerFrame.setBlocking(True)
+objectTracker.inputDetectionFrame.setBlocking(True)
+objectTracker.inputDetections.setBlocking(True)
+objectTracker.setDetectionLabelsToTrack([0])  # track only car
+# possible tracking types: ZERO_TERM_COLOR_HISTOGRAM, ZERO_TERM_IMAGELESS, SHORT_TERM_IMAGELESS, SHORT_TERM_KCF
+objectTracker.setTrackerType(dai.TrackerType.SHORT_TERM_IMAGELESS)
+# take the smallest ID when new object is tracked, possible options: SMALLEST_ID, UNIQUE_ID
+objectTracker.setTrackerIdAssignmentPolicy(dai.TrackerIdAssignmentPolicy.SMALLEST_ID)
+
+# Linking
+manip.out.link(manipOut.input)
+manip.out.link(detectionNetwork.input)
+xinFrame.out.link(manip.inputImage)
+xinFrame.out.link(objectTracker.inputTrackerFrame)
+detectionNetwork.out.link(nnOut.input)
+detectionNetwork.out.link(objectTracker.inputDetections)
+detectionNetwork.passthrough.link(objectTracker.inputDetectionFrame)
+objectTracker.out.link(trackerOut.input)
+objectTracker.passthroughTrackerFrame.link(xlinkOut.input)
+
 with dai.Device(pipeline) as device:
 
-    # Input queue will be used to send video frames to the device.
     qIn = device.getInputQueue(name="inFrame")
-    # Output queue will be used to get nn data from the video frames.
-    qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
+    trackerFrameQ = device.getOutputQueue(name="trackerFrame", maxSize=4)
+    tracklets = device.getOutputQueue(name="tracklets", maxSize=4)
+    qManip = device.getOutputQueue(name="manip", maxSize=4)
+    qDet = device.getOutputQueue(name="nn", maxSize=4)
 
-    frame = None
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
     detections = []
+    frame = None
+
+    def to_planar(arr: np.ndarray, shape: tuple) -> np.ndarray:
+        return cv2.resize(arr, shape).transpose(2, 0, 1).flatten()
 
     # nn data, being the bounding box locations, are in <0..1> range - they need to be normalized with frame width/height
     def frameNorm(frame, bbox):
@@ -110,53 +145,76 @@ with dai.Device(pipeline) as device:
         normVals[::2] = frame.shape[1]
         return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
 
-    def to_planar(arr: np.ndarray, shape: tuple) -> np.ndarray:
-        return cv2.resize(arr, shape).transpose(2, 0, 1).flatten()
-
     def displayFrame(name, frame):
         for detection in detections:
             bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
+            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
             cv2.putText(frame, labelMap[detection.label], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
             cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-        # Show the frame
         cv2.imshow(name, frame)
 
     cap = cv2.VideoCapture(args.video)
-    frame_counter = 0
-    target_fps = 30
-    target_frame_time = 1.0 / target_fps
+    baseTs = time.monotonic()
+    simulatedFps = 30
+    inputFrameShape = (W, H)
+
     while cap.isOpened():
-        start = time.time()
         read_correctly, frame = cap.read()
         if not read_correctly:
             break
-        
-        frame_counter += 1
-        #If the last frame is reached, reset the capture and the frame_counter
-        if frame_counter == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            frame_counter = 0 #Or whatever as long as it is the same as next line
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                
+
         img = dai.ImgFrame()
-        img.setData(to_planar(frame, (768, 416)))
-        img.setTimestamp(monotonic())
-        img.setWidth(768)
-        img.setHeight(416)
+        img.setType(dai.ImgFrame.Type.BGR888p)
+        img.setData(to_planar(frame, inputFrameShape))
+        img.setTimestamp(baseTs)
+        baseTs += 1/simulatedFps
+
+        img.setWidth(inputFrameShape[0])
+        img.setHeight(inputFrameShape[1])
         qIn.send(img)
 
-        inDet = qDet.tryGet()
+        trackFrame = trackerFrameQ.tryGet()
+        if trackFrame is None:
+            continue
 
-        if inDet is not None:
-            detections = inDet.detections
+        track = tracklets.get()
+        manip = qManip.get()
+        inDet = qDet.get()
 
-        if frame is not None:
-            displayFrame("rgb", frame)
+        counter+=1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1 :
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time
+
+        detections = inDet.detections
+        # manipFrame = manip.getCvFrame()
+        # displayFrame("nn", manipFrame)
+
+        color = (255, 0, 0)
+        trackerFrame = trackFrame.getCvFrame()
+        trackletsData = track.tracklets
+        for t in trackletsData:
+            roi = t.roi.denormalize(trackerFrame.shape[1], trackerFrame.shape[0])
+            x1 = int(roi.topLeft().x)
+            y1 = int(roi.topLeft().y)
+            x2 = int(roi.bottomRight().x)
+            y2 = int(roi.bottomRight().y)
+
+            try:
+                label = labelMap[t.label]
+            except:
+                label = t.label
+
+            cv2.putText(trackerFrame, str(label), (x1, y1), cv2.FONT_HERSHEY_TRIPLEX, 0.3, 120)
+            # cv2.putText(trackerFrame, f"ID: {[t.id]}", (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            # cv2.putText(trackerFrame, t.status.name, (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+            cv2.rectangle(trackerFrame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
+
+        cv2.putText(trackerFrame, "Fps: {:.2f}".format(fps), (2, trackerFrame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
+
+        cv2.imshow("tracker", trackerFrame)
 
         if cv2.waitKey(1) == ord('q'):
             break
-        
-        end = time.time()
-        frame_time = end - start
-        if frame_time < target_frame_time:
-            time.sleep(target_frame_time - frame_time)
